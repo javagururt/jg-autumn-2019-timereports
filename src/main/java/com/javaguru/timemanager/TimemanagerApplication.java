6e6005ff669eafb5f2348dac908adfb40dd6cdc2
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2019-12-02 08:39:38 +0200

    first commit

---
+++ src/main/java/com/javaguru/timemanager/TimemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.javaguru.timemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimemanagerApplication.class, args);
+	}
+
+}
